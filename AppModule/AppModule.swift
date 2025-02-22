5b3fe46f4d600be858c704357ad76ad93c6f2907
Saatvik Shashank Shrivastava <68585473+SaatvikSS@users.noreply.github.com>
2025-02-22 21:11:07 +0530

    Update AppModule.swift

--- AppModule/AppModule.swift
+++ AppModule/AppModule.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 @main
-struct MyApp: App {
+struct NutriScanApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreen()
         }
     }
 }
